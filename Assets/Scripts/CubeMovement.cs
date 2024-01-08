f8cffa671a3095ccd15c098dc404a4091cd165c3
felixlaterrot <felixlaterrot@gmail.com>
2024-01-08 16:19:33 +0100

    life system

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -1,5 +1,8 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CubeMovement : MonoBehaviour
 {
@@ -7,14 +10,32 @@ public class CubeMovement : MonoBehaviour
     float _speed;
 
     public Text phaseDisplayText;
+    public bool[] life;
+    public GameObject[] lifeDisplay;
     private Vector2 touchStartPos;
+
+    [SerializeField] private GameObject lifePrefab;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        life = new bool[4];
+        for (int i = 0; i < life.Length; i++)
+        {
+            life[i] = true;
+        }
+
+        Color color = Color.black;
+        lifeDisplay = new GameObject[4];
+        for (int i = 0; i < lifeDisplay.Length; i++)
+        {
+            lifeDisplay[i] = Instantiate(lifePrefab, new Vector3(-9.63f + i, 4f, 0f), Quaternion.identity);
+        }
         _speed = 5f;
         transform.position = new Vector2(0, 0);
+
+        DisplayLife();
     }
 
     // Update is called once per frame
@@ -43,7 +64,42 @@ public class CubeMovement : MonoBehaviour
     {
         if (collision.gameObject.name == "Bullet(Clone)")
         {
-            
+            int index = life.Length - 1; // position du dernier true dans le tableau
+            for (int i = 0; i < life.Length; i++)
+            {
+                if (!life[i])
+                {
+                    index = i - 1;
+                    break;
+                }
+            }
+            life[index] = false; //retire une vie
+            if (!life[0])
+            {
+                Retry();
+            }
+               
+            DisplayLife();
+        }
+    }
+
+    public void Retry()
+    {
+        //Restarts current level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void DisplayLife()
+    {
+        int i = 0;
+        foreach (var item in life)
+        {
+            if (!item)
+            {
+                Debug.Log(item);
+                lifeDisplay[i].GetComponent<SpriteRenderer>().material.color = Color.white;
+            }
+            i += 1;
         }
     }
 
---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+
+    public int life;
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
